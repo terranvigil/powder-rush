3db7c9a356826015b28fa4b958292638874df043
Terran Vigil <terranvigil@gmail.com>
2026-03-03 21:36:18 -0500

    Updates

---
+++ assets/color.vertex-CRNIVfE6.js
@@ -0,0 +1,32 @@
+import{S as i}from"./index-B_Xi_edF.js";import"./bakedVertexAnimation-L5X46aoO.js";import"./clipPlaneVertex-BiIQPKPV.js";import"./fogVertex-DdQAAmER.js";import"./instancesDeclaration-Bsb5Lb-U.js";import"./vertexColorMixing-CJ3sy3oE.js";const e="colorVertexShader",n=`attribute position: vec3f;
+#ifdef VERTEXCOLOR
+attribute color: vec4f;
+#endif
+#include<bonesDeclaration>
+#include<bakedVertexAnimationDeclaration>
+#include<clipPlaneVertexDeclaration>
+#include<fogVertexDeclaration>
+#ifdef FOG
+uniform view: mat4x4f;
+#endif
+#include<instancesDeclaration>
+uniform viewProjection: mat4x4f;
+#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+varying vColor: vec4f;
+#endif
+#define CUSTOM_VERTEX_DEFINITIONS
+@vertex
+fn main(input : VertexInputs)->FragmentInputs {
+#define CUSTOM_VERTEX_MAIN_BEGIN
+#ifdef VERTEXCOLOR
+var colorUpdated: vec4f=vertexInputs.color;
+#endif
+#include<instancesVertex>
+#include<bonesVertex>
+#include<bakedVertexAnimation>
+var worldPos: vec4f=finalWorld* vec4f(input.position,1.0);vertexOutputs.position=uniforms.viewProjection*worldPos;
+#include<clipPlaneVertex>
+#include<fogVertex>
+#include<vertexColorMixing>
+#define CUSTOM_VERTEX_MAIN_END
+}`;i.ShadersStoreWGSL[e]||(i.ShadersStoreWGSL[e]=n);const f={name:e,shader:n};export{f as colorVertexShaderWGSL};
---
+++ assets/kernelBlurVaryingDeclaration-CtvQJIQ-.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index-B_Xi_edF.js";const r="kernelBlurVaryingDeclaration",a="varying vec2 sampleCoord{X};";e.IncludesShadersStore[r]||(e.IncludesShadersStore[r]=a);
